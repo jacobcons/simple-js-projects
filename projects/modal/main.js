e3024cc1f6b10e5f74faa8c65b03cf8d7a2a4d69
Jacob <jacobcons@gmail.com>
2023-11-10 16:05:52 +0000

    modal finished

---
+++ projects/modal/main.js
@@ -0,0 +1,11 @@
+import { $, $$ } from '../lib.js';
+
+const openModalButton = $('.js-open-modal-button');
+const closeModalButton = $('.js-close-modal-button');
+const modal = $('.js-modal');
+openModalButton.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+closeModalButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
